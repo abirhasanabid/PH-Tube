376452ca234ed0e9807153fd89d2342319e82faa
Abid Hasan Abir <abid.7.tm@gmial.com>
2024-10-04 09:51:36 +0600

    added some working btn function

---
+++ script/test.js
@@ -0,0 +1,14 @@
+const isVarified = 'abid';
+// if(isVarified){
+//     console.log('verified');
+
+// }else{
+//     console.log('not varified');
+
+// }
+
+// const result = isVarified? 'verified':'not verified';
+// console.log(result);
+
+
+
--- script/vedio.js
+++ script/vedio.js
@@ -14,41 +14,42 @@ const videoCatagory = () => {
         .catch(err => console.error(err))
 }
 
-// display  btnCategory
+// vedio time
+function fidnTime(time) {
+    const hour = parseInt(time / 3600);
+    const remainingSecond = time % 3600;
+    const min = parseInt(remainingSecond / 60);
+    return `${hour} hour ${min} min ago`;
+}
+
+// click btn and find vedios
+const loadVideos = (id) => {
+    fetch(`https://openapi.programming-hero.com/api/phero-tube/category/${id}`)
+        .then(res => res.json())
+        .then(data => displayVedioCatagory(data.category))
+        .catch(err => console.error(err))
+}
+
+
+// display btnCategory
 const displayCatagory = (catagories) => {
     const btnDiv = document.getElementById('btnDiv');
     catagories.forEach(item => {
-        const btn = document.createElement('button');
-        btn.classList = 'btn';
-        btn.innerText = item.category;
-        btnDiv.append(btn);
+        const btnContainer = document.createElement('div');
+        btnContainer.innerHTML = `
+        <button onclick="loadVideos(${item.category_id})" class="btn">
+        ${item.category}
+        </button>
+        `
+        btnDiv.append(btnContainer);
 
     })
 }
 
-
-
-// const copyObj = {
-//     "category_id": "1001",
-//     "video_id": "aaad",
-//     "thumbnail": "https://i.ibb.co/f9FBQwz/smells.jpg",
-//     "title": "Smells Like Teen Spirit",
-//     "authors": [
-//         {
-//             "profile_picture": "https://i.ibb.co/k4tkc42/oliviar-harris.jpg",
-//             "profile_name": "Oliver Harris",
-//             "verified": true
-//         }
-//     ],
-//     "others": {
-//         "views": "5.4K",
-//         "posted_date": "1672656000"
-//     },
-//     "description": "'Smells Like Teen Spirit' by Oliver Harris captures the raw energy and rebellious spirit of youth. With over 5.4K views, this track brings a grunge rock vibe, featuring powerful guitar riffs and compelling vocals. Oliver's verified profile guarantees a quality musical journey that resonates with fans of dynamic, high-energy performances."
-// }
 // display VedionCategory
 const displayVedioCatagory = (data) => {
     const vediosContainer = document.getElementById('vediosContainer');
+    vediosContainer.innerHTML = '';
     data.forEach(item => {
         console.log(item);
 
@@ -56,21 +57,24 @@ const displayVedioCatagory = (data) => {
         card.classList = 'card card-compact';
         card.innerHTML =
             `
-        <figure class="h-[220px]">
-    <img class="w-full h-full object-cover rounded-md" 
-      src=${item.thumbnail}
-      alt="Shoes"/>
-  </figure>
+        <figure class="h-[220px] relative">
+           <img class="w-full h-full object-cover rounded-md" 
+            src=${item.thumbnail}
+            alt="Shoes"/>
+            ${item.others.posted_date?.length == 0 ? '' : `<span class="absolute right-2 bottom-2 bg-gray-950 text-white p-1 rounded-lg"> ${fidnTime(item.others.posted_date)}</span>`}
+           
+       </figure>
   <div class="py-2">
     <div class="flex gap-2">
        <div>
          <img class="w-10 h-10 rounded-full object-cover" src="${item.authors[0].profile_picture}" alt="">
         </div>
         <div>
-          <h2 class="card-title font-bold">${item.title}</h2>
-          <div class="flex items-center gap-2">
+            <h2 class="card-title font-bold">${item.title}</h2>
+            <div class="flex items-center gap-2">
             <p class="text-gray-500">${item.authors[0].profile_name}</p>
-            <img class="w-5 h-5" src="https://img.icons8.com/?size=48&id=yXOHowNYbgM5&  format=png" alt="">
+            ${item.authors[0].verified ? '<img class="w-5 h-5" src="https://img.icons8.com/?size=48&id=yXOHowNYbgM5&  format=png" alt="">' : ''}
+            
           </div>
           <p class="text-gray-500">${item.others.views} views</P>
        </div>
